68f2f722722617dccfc72ba36646c498abfd1292
JSpeed8th <jspeed8th@gmail.com>
2018-01-17 00:11:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,18 +4,6 @@ function BoardMember (name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  this.approve = function () {
-    return 'You can do that!'
-  }
-  this.doCharity = function () {
-    return 'I like to help people.'
-  }
-  this.releasePressStatement = function () {
-    return 'You will see great things from Scuber.'
-  }
-  this.sayHi = function () {
-    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
-  }
 
 }
 
@@ -23,6 +11,21 @@ BoardMember.prototype.veto = function () {
   return 'No, I must disagree'
 }
 
+BoardMember.prototype.approve = function () {
+  return 'You can do that!'
+}
+
+BoardMember.prototype.doCharity = function () {
+  return 'I like to help people.'
+}
+
+BoardMember.prototype.releasePressStatement = function () {
+  return 'You will see great things from Scuber.'
+}
+
+BoardMember.prototype.sayHi = function () {
+  return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
+}
 /*+ `veto` — returns `No, I must disagree`
 
 + `approve` — returns `You can do that!`
